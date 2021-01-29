1aacfdd90e47ac599f902379a496eaa02dfd14d3
Fsesnick <fernandasesnick@gmail.com>
2021-01-29 23:33:10 +0000

    Refactor 2 logica dos if else

--- app.js
+++ app.js
@@ -75,31 +75,39 @@ ul.addEventListener('change', (e) => {
   }
 });
   
-ul.addEventListener('click', (e) => {
-  if (e.target.tagName === 'BUTTON') {
-    const button = e.target;
-    const li = button.parentNode;
-    const ul = li.parentNode;
-    if (button.textContent === 'remove') {
-      ul.removeChild(li);
-    } else if (button.textContent === 'edit') { 
-      const span = li.firstElementChild;
-      const input = document.createElement('input');
-      input.type = 'text';
-      input.value = span.textContent;
-      li.insertBefore(input, span);
-      li.removeChild(span);
-      button.textContent = 'save';
-    } else if (button.textContent === 'save') { 
-      const input = li.firstElementChild;
-      const span = document.createElement('span');
-      span.textContent = input.value;
-      li.insertBefore(span, input);
-      li.removeChild(input);
-      button.textContent = 'edit';
+ ul.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+      const button = e.target;
+      const li = button.parentNode;
+      const ul = li.parentNode;
+      const action = button.textContent;
+      const nameActions = {
+        remove: () => {
+          ul.removeChild(li);
+        },
+        edit: () => {
+          const span = li.firstElementChild;
+          const input = document.createElement('input');
+          input.type = 'text';
+          input.value = span.textContent;
+          li.insertBefore(input, span);
+          li.removeChild(span);
+          button.textContent = 'save';  
+        },
+        save: () => {
+          const input = li.firstElementChild;
+          const span = document.createElement('span');
+          span.textContent = input.value;
+          li.insertBefore(span, input);
+          li.removeChild(input);
+          button.textContent = 'edit';        
+        }
+      };
+      
+      // selecionar e executar a ação no nome do botão
+      nameActions[action]();
     }
-  }
-});  
+  });   
 
 });  
   
