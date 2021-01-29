c02fc5aafa26ec5120c978a26fd355b2fd8a4378
Fsesnick <fernandasesnick@gmail.com>
2021-01-29 10:59:59 +0000

    add text content

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const input = form.querySelector('input');
 form.addEventListener('submit', (e) => {
 
   e.preventDefault();
-  console.log(input.value);
+  const text = input.value;
   const ul = document.getElementById('invitedList');
   const li = document.createElement('li');
+  li.textContent = text;
   ul.appendChild(li);
 });
