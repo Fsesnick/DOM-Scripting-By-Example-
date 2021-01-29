eafba1e41ced9565880b8e6ea01cdf425820a941
Fsesnick <fernandasesnick@gmail.com>
2021-01-29 16:51:47 +0000

    Salvando nomes editados

--- app.js
+++ app.js
@@ -55,15 +55,24 @@ ul.addEventListener('click', (e) =>{
     
     if( button.textContent === 'remove'){
       ul.removeChild(li);      
+   
     }else if(button.textContent === 'edit'){
-      //console.log('edit');
-       const span = li.firstElementChild;
-       const input = document.createElement('input');
+     
+      const span = li.firstElementChild;
+      const input = document.createElement('input');
       input.type ='text';
       input.value = span.textContent;
       li.insertBefore(input,span);
       li.removeChild(span);
       button.textContent = 'save';
+   
+    }else if(button.textContent === 'save'){
+      const input = li.firstElementChild;
+      const span = document.createElement('span');
+      span.textContent = input.value
+      li.insertBefore(span,input);
+      li.removeChild(input);
+      button.textContent = 'edit';
     }
   }
 });
